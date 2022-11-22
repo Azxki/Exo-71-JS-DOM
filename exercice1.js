36fd153beb32cc57d188fe8f91d3262aa13caad8
azxki <cococoursan@gmail.com>
2022-11-22 11:26:02 +0100

    EXO

--- exercice1.js
+++ exercice1.js
@@ -1,7 +1,18 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+let monDiv2 = document.createElement('div');
+let viewport = document.getElementById('viewport');
 
-document.getElementById('viewport').appendChild(monDiv);
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
+monDiv2.innerHTML = "Div numéro 2"
 
+viewport.appendChild(monDiv);
+viewport.appendChild(monDiv2);
+
+monDiv2.style.backgroundColor = "red";
+monDiv2.style.width = "100" + "px";
+monDiv2.style.height = "100" + "px";
+
+document.getElementById('aSupprimer').remove();
 
 
 
